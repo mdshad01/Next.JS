0e32d5e917fb5f3ab4d5ce5804be22090e210830
Shad <shadalam0024@gmail.com>
2025-08-04 21:44:42 +0530

    saas

--- section-4.1/app/layout.js
+++ section-4.1/app/layout.js
@@ -5,12 +5,6 @@ import "./globals.css";
 export const dynamic = "force-dynamic";
 
 export default function RootLayout({ children }) {
-  const randomNumber = Math.random();
-  console.log(randomNumber);
-
-  if (randomNumber > 0.5) {
-    throw new Error("Error occurred");
-  }
   return (
     <html lang="en" className="dark">
       <body>
--- section-4.1/app/page.js
+++ section-4.1/app/page.js
@@ -1,17 +1,19 @@
 export const dynamic = "force-dynamic";
+import styles from "./home.module.css";
+import "./home.scss";
 
 const Home = () => {
-  const randomNumber = Math.random();
-  console.log(randomNumber);
+  // const randomNumber = Math.random();
+  // console.log(randomNumber);
 
-  if (randomNumber > 0.5) {
-    throw new Error("Error occurred");
-  }
+  // if (randomNumber > 0.5) {
+  //   throw new Error("Error occurred");
+  // }
 
   return (
     <>
-      <div>
-        <h1>Home Page</h1>
+      <div className={styles.home}>
+        <h1 className={styles.title}>Home Page</h1>
         <p>Welcome to our website!</p>
       </div>
     </>
